f42532d9504c72c5059e3b3876f3814610f22dfc
Maxime <maxime.pie.mail@gmail.com>
2019-05-17 22:35:21 +0200

    feat(carlos) - Add dynamic apparition of bottom text

--- public/js/app.js
+++ public/js/app.js
@@ -61624,62 +61624,60 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _images_homepage_wallpaper_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../images/homepage/wallpaper.jpg */ "./resources/images/homepage/wallpaper.jpg");
 /* harmony import */ var _images_homepage_wallpaper_jpg__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_images_homepage_wallpaper_jpg__WEBPACK_IMPORTED_MODULE_2__);
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
 
+function Homepage() {
+  var landingText = [{
+    text: "De direction ?",
+    timeout: 500
+  }, {
+    text: "De vie ?",
+    timeout: 500
+  }, {
+    text: "De pensée ?",
+    timeout: 500
+  }, {
+    text: "Le monde ?",
+    timeout: 2000
+  }];
 
-var Homepage =
-/*#__PURE__*/
-function (_Component) {
-  _inherits(Homepage, _Component);
+  var _React$useState = react__WEBPACK_IMPORTED_MODULE_0___default.a.useState(1),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      textSliderCurrentIndex = _React$useState2[0],
+      setTextSliderCurrentIndex = _React$useState2[1];
 
-  function Homepage() {
-    _classCallCheck(this, Homepage);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(Homepage).apply(this, arguments));
-  }
-
-  _createClass(Homepage, [{
-    key: "render",
-    value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "Homepage"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "Homepage-landing"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
-        className: "Homepage-landing__header"
-      }, "Et si on changeait..."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        className: "Homepage-landing__image",
-        alt: "Fond d'\xE9cran",
-        src: _images_homepage_wallpaper_jpg__WEBPACK_IMPORTED_MODULE_2___default.a
-      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
-        className: "Homepage-landing__header"
-      }, "Le monde ?")));
-    }
-  }]);
-
-  return Homepage;
-}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
-
-
+  react__WEBPACK_IMPORTED_MODULE_0___default.a.useEffect(function () {
+    setTimeout(function () {
+      if (textSliderCurrentIndex === landingText.length - 1) {
+        setTextSliderCurrentIndex(0);
+      } else {
+        setTextSliderCurrentIndex(textSliderCurrentIndex + 1);
+      }
+    }, landingText[textSliderCurrentIndex].timeout);
+  }, [textSliderCurrentIndex]);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "Homepage"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "Homepage-landing"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "Homepage-landing__header"
+  }, "Et si on changeait..."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "Homepage-landing__image",
+    alt: "Fond d'\xE9cran",
+    src: _images_homepage_wallpaper_jpg__WEBPACK_IMPORTED_MODULE_2___default.a
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "Homepage-landing__header Homepage-landing__header--slider"
+  }, landingText[textSliderCurrentIndex].text)));
+}
 
 if (document.getElementById('root')) {
   react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Homepage, null), document.getElementById('root'));
--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -2,18 +2,39 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import landingBackgroundImage from "../../images/homepage/wallpaper.jpg"
 
-export default class Homepage extends Component {
-  render() {
-    return (
-      <div className="Homepage">
-        <div className="Homepage-landing">
-          <h1 className="Homepage-landing__header">Et si on changeait...</h1>
-          <img className="Homepage-landing__image" alt="Fond d'écran" src={landingBackgroundImage}/>
-          <h1 className="Homepage-landing__header">Le monde ?</h1>
-        </div>
+export default function Homepage(){
+  const landingText = [
+    {text: "De direction ?", timeout: 500},
+    {text: "De vie ?", timeout: 500},
+    {text: "De pensée ?", timeout: 500},
+    {text: "Le monde ?", timeout: 2000},
+  ];
+  const [textSliderCurrentIndex, setTextSliderCurrentIndex] = React.useState(1);
+
+  React.useEffect( () =>{
+    setTimeout(() => {
+      if (textSliderCurrentIndex === landingText.length - 1) {
+        setTextSliderCurrentIndex(0)
+      }
+      else {
+        setTextSliderCurrentIndex(textSliderCurrentIndex + 1)
+      }
+    }, landingText[textSliderCurrentIndex].timeout)
+  }, [textSliderCurrentIndex]);
+
+  return (
+    <div className="Homepage">
+      <div className="Homepage-landing">
+        <h1 className="Homepage-landing__header">Et si on changeait...</h1>
+        <img className="Homepage-landing__image" alt="Fond d'écran" src={landingBackgroundImage}/>
+        <h1
+          className="Homepage-landing__header Homepage-landing__header--slider"
+        >
+          {landingText[textSliderCurrentIndex].text}
+        </h1>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 if (document.getElementById('root')) {
